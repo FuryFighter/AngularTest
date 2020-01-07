befebf1b4c7310aabf915c439e1875bef3b72f28
FuryFighter <klotzcraftlp@googlemail.com>
2020-01-07 11:52:09 +0100

    Initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: '#MagentaCrew Phone Pro+',
+    price: 799,
+    description: 'A large phone with one of the best screens and cameras'
+  },
+  {
+    name: '#MagentaCrew Phone Pro',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: '#MagentaCrew Phone',
+    price: 499,
+    description: 'Ja lol ey'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
